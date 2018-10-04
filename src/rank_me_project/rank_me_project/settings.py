73def2ffab9382563b2306c322bd0034147e3dfa
Rafal Ozog <rafalozog@gmail.com>
2018-10-04 19:51:50 +0100

    Path bug - fix try

--- src/rank_me_project/rank_me_project/settings.py
+++ src/rank_me_project/rank_me_project/settings.py
@@ -121,8 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# does it work?
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 #ENV_PATH = os.path.abspath(os.path.dirname(__file__))
 #MEDIA_ROOT = os.path.join(ENV_PATH, 'media/')

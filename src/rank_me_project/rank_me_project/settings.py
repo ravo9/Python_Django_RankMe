2c269b77afd550e9ed403761f44078d1e424a55f
Rafal Ozog <rafalozog@gmail.com>
2018-10-05 06:00:49 +0100

    Static files issue - fix try.

--- src/rank_me_project/rank_me_project/settings.py
+++ src/rank_me_project/rank_me_project/settings.py
@@ -121,13 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-if DEBUG:
-    STATIC_ROOT = '/usr/local/apps/rankme-api/src/rank_me_project/'
-    STATICFILES_DIRS = (
-        '/usr/local/apps/rankme-api/src/rank_me_project/static/',
-    )
-
 #STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 #STATIC_ROOT = os.path.join(BASE_DIR, "static")
 

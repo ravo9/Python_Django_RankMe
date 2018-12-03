4699585a3df8b46c8df99312038eb2a595840e7a
Rafal Ozog <rafalozog@gmail.com>
2018-12-03 06:19:03 +0000

    update

--- src/rank_me_project/rank_me_api/serializers.py
+++ src/rank_me_project/rank_me_api/serializers.py
@@ -31,8 +31,8 @@ class ProfileSerializer(serializers.ModelSerializer):
         return user
 
     def partial_update(self, instance, validated_data):
-        instance.set_password(instance.password)
-        instance.save()
+        validated_data['password'] = set_password(validated_data['password'])
+        validated_data.save()
         return super(ProfileSerializer, self).update(instance, validated_data)
         #instance.email = validated_data.get('email', instance.email)
         #instance.name = validated_data.get('name', instance.name)

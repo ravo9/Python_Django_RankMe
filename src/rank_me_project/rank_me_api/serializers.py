4b5b26ee4af710eecdb3f747fe1bc20e656fd5e8
Rafal Ozog <rafalozog@gmail.com>
2018-12-09 11:58:31 +0000

    Update

--- src/rank_me_project/rank_me_api/serializers.py
+++ src/rank_me_project/rank_me_api/serializers.py
@@ -33,17 +33,19 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         if ('password' in validated_data):
-            validated_data['password'].set_password(validated_data['password'])
-            #instance.set_password(validated_data['password'])
-            #instance.save()
-        return super().update(instance, validated_data)
+            instance.set_password(validated_data['password'])
+            instance.save()
+            return instance
+        else:
+            return super().update(instance, validated_data)
 
     def partial_update(self, instance, validated_data):
         if ('password' in validated_data):
-            validated_data['password'].set_password(validated_data['password'])
-            #instance.set_password(validated_data['password'])
-            #instance.save()
-        return super().partial_update(instance, validated_data)
+            instance.set_password(validated_data['password'])
+            instance.save()
+            return instance
+        else:
+            return super().update(instance, validated_data)
 
         #validated_data['password'] = set_password(validated_data['password'])
         #validated_data.save()

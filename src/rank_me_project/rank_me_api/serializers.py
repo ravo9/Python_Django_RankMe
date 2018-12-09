712c150223d8830149234e9cc9b18487fa755506
Rafal Ozog <rafalozog@gmail.com>
2018-12-09 11:38:00 +0000

    Update

--- src/rank_me_project/rank_me_api/serializers.py
+++ src/rank_me_project/rank_me_api/serializers.py
@@ -35,12 +35,14 @@ class ProfileSerializer(serializers.ModelSerializer):
         if ('password' in validated_data):
             instance.set_password(validated_data['password'])
             instance.save()
+        super.update(self, instance, validated_data)
         return instance
 
     def partial_update(self, instance, validated_data):
         if ('password' in validated_data):
             instance.set_password(validated_data['password'])
             instance.save()
+        super.partial_update(self, instance, validated_data)
         return instance
 
         #validated_data['password'] = set_password(validated_data['password'])

53be1bdb585b2d0f9ba46f1dcc1e7744c71a76ea
Rafal Ozog <rafalozog@gmail.com>
2019-01-14 22:13:43 +0000

    Update

--- src/rank_me_project/rank_me_api/views.py
+++ src/rank_me_project/rank_me_api/views.py
@@ -79,7 +79,7 @@ class RandomPictureItemViewSet(viewsets.ModelViewSet):
         return self.kwargs['pk']
 
     def get_queryset(self):
-        queryset = queryset.exclude(profile__id == 67)
+        queryset = queryset.exclude(profile__id=67)
         random_picture = random.choice(self.queryset)
         return [random_picture]
 

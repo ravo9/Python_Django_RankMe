d64a21b2418fa7212d6ac83efaff2d4cc0ad941e
Rafal Ozog <rafalozog@gmail.com>
2019-01-14 20:55:40 +0000

    Update

--- src/rank_me_project/rank_me_api/views.py
+++ src/rank_me_project/rank_me_api/views.py
@@ -79,12 +79,6 @@ class RandomPictureItemViewSet(viewsets.ModelViewSet):
         random_picture = random.choice(self.queryset)
         return [random_picture]
 
-    def get_queryset(self, user_id):
-        random_picture = random.choice(self.queryset)
-        if (random_picture.id == user_id):
-            random_picture = get_queryset(self, user_id)
-        return [random_picture]
-
 
 class GradeItemViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)

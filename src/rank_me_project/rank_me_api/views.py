74298166e69492d1636c8dbf58e37ff94d35284e
Rafal Ozog <rafalozog@gmail.com>
2019-01-16 22:17:44 +0000

    Update

--- src/rank_me_project/rank_me_api/views.py
+++ src/rank_me_project/rank_me_api/views.py
@@ -80,7 +80,7 @@ class RandomPictureItemViewSet(viewsets.ModelViewSet):
         filtered_queryset = self.queryset.filter(~Q(profile=self.request.user))
         if (len(pictures_already_graded) > 0):
             for grade in pictures_already_graded:
-                filtered_queryset = filtered_queryset.filter(~Q(profile=grade__grading_profile))
+                filtered_queryset = filtered_queryset.filter(~Q(profile.id=grade.grading_profile))
         #random_picture = random.choice(filtered_queryset)
         #return [random_picture]
         filtered_queryset_ids = []

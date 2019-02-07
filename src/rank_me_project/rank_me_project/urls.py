e7c6fa8ae2e42e45626ab491c378ffc749ffd3a6
Rafal Ozog <rafalozog@gmail.com>
2019-02-07 00:59:41 +0000

    Reset Password Implementation Update

--- src/rank_me_project/rank_me_project/urls.py
+++ src/rank_me_project/rank_me_project/urls.py
@@ -4,7 +4,10 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
-from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm
+from django.contrib.auth.views import  (
+    password_reset, password_reset_done, password_reset_confirm,
+    password_reset_complete
+)
 
 router = DefaultRouter()
 router.register('profiles', views.ProfileViewSet)
@@ -19,7 +22,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^reset-password/$', password_reset, name='reset_password'),
     url(r'^reset-password/done/$', password_reset_done, name='password_reset_done'),
-    url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, name='password_reset_confirm')
+    url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset-password/complete/$', password_reset_complete, name='password_reset_complete')
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

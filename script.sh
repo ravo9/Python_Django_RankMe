5c9224d9b93a3f4fe398520bf5f6ae79a37958d9
Rafal Ozog <rafalozog@gmail.com>
2019-02-21 22:07:28 +0000

    Update

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
-echo >> "FlagTest 02"
+echo "FlagTest 02"
 cd /usr/local/apps/rankme-api
-echo >> "FlagTest 03"
+echo "FlagTest 03"
 git pull
-echo >> "FlagTest 04"
+echo "FlagTest 04"
 supervisorctl restart all
-echo >> "FlagTest 05"
+echo "FlagTest 05"

373011826c21a89c979dd68e31ddd89d943496b6
Rafal Ozog <rafalozog@gmail.com>
2019-02-21 22:01:09 +0000

    Update

--- script.sh
+++ script.sh
@@ -1,4 +1,9 @@
+echo >> "FlagTest 01"
 sudo -i
+echo >> "FlagTest 02"
 cd /usr/local/apps/rankme-api
+echo >> "FlagTest 03"
 git pull
+echo >> "FlagTest 04"
 supervisorctl restart all
+echo >> "FlagTest 05"

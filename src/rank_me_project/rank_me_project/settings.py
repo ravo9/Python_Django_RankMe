dbe3236cb75600e30aae1316db126533fb36d126
Rafal Ozog <rafalozog@gmail.com>
2019-02-21 19:29:15 +0000

    New allowed host address.

--- src/rank_me_project/rank_me_project/settings.py
+++ src/rank_me_project/rank_me_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w6ry&h9*7d*n*yh&a!94y)17xsm%d*39mjxd^d4h!9i=870btq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-18-223-109-148.us-east-2.compute.amazonaws.com']
+ALLOWED_HOSTS = ['rank-me.co.uk']
 
 # Application definition
 

6c16d47ca7f51c30a99a4d275ceebd9cd6d44ace
Rafal Ozog <rafalozog@gmail.com>
2019-02-26 08:56:12 +0000

    Update

--- src/rank_me_project/rank_me_project/settings.py
+++ src/rank_me_project/rank_me_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w6ry&h9*7d*n*yh&a!94y)17xsm%d*39mjxd^d4h!9i=870btq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rankMeLoadBalancer-378788423.us-east-2.elb.amazonaws.com']
+ALLOWED_HOSTS = ['https://rank-me.co.uk']
 
 # Application definition
 

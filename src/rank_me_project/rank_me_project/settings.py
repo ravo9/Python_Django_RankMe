255a9520f7ba6ef8d9cb0c1050b4c0bca96ed28d
Rafal Ozog <rafalozog@gmail.com>
2019-02-27 09:33:25 +0000

    Update

--- src/rank_me_project/rank_me_project/settings.py
+++ src/rank_me_project/rank_me_project/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w6ry&h9*7d*n*yh&a!94y)17xsm%d*39mjxd^d4h!9i=870btq'
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

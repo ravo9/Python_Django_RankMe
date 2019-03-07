33e926c8f7f3c56f0019b5abb08499db81ef63e2
Rafal Ozog <rafalozog@gmail.com>
2019-03-07 19:27:23 +0000

    Update

--- src/rank_me_project/rank_me_project/settings.py
+++ src/rank_me_project/rank_me_project/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://rank-me.co.uk/media/'
+#MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 
 AUTH_USER_MODEL = 'rank_me_api.UserProfile'

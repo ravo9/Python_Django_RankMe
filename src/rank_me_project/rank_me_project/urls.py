c2668915701e340341967865083016c6a3672324
Rafal Ozog <rafalozog@gmail.com>
2019-03-07 12:16:12 +0000

    Update

--- src/rank_me_project/rank_me_project/urls.py
+++ src/rank_me_project/rank_me_project/urls.py
@@ -26,7 +26,8 @@ urlpatterns = [
     url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, name='password_reset_confirm'),
     url(r'^reset-password/complete/$', password_reset_complete, name='password_reset_complete'),
     url(r'^download/(?P<file_name>.+)$', views.download),
-    url(r'^.well-known/pki-validation/(?P<file_name>.+)$', views.download)
+    url(r'^.well-known/pki-validation/(?P<file_name>.+)$', views.download),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'media'})
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

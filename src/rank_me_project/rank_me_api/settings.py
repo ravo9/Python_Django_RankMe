24fa459fcab50c6eb7a27a9aa24847456b2bb075
Rafal Ozog <rafalozog@gmail.com>
2019-03-07 20:09:22 +0000

    Update

--- src/rank_me_project/rank_me_api/settings.py
+++ src/rank_me_project/rank_me_api/settings.py
@@ -3,3 +3,5 @@ REST_AUTH_SERIALIZERS = {
     'PASSWORD_RESET_SERIALIZER':
         'rank_me_api.serializers.PasswordResetSerializer',
 }
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')

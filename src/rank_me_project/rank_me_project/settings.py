38e151921e0d633971a4f8db1907da26359f36a0
Rafal Ozog <rafalozog@gmail.com>
2019-03-07 01:53:07 +0000

    Update

--- src/rank_me_project/rank_me_project/settings.py
+++ src/rank_me_project/rank_me_project/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ['rank-me.co.uk']
 
 # HTTP -> HTTPS Redirection
 SECURE_SSL_REDIRECT = True
+USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
